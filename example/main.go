c5ac8dfd38e5998d546b7c5ebf9a78f03bc39eca
Amaury Diaz <amauryuh@gmail.com>
2024-04-30 14:20:11 +0200

    Release v0.0.31

--- example/main.go
+++ example/main.go
@@ -6,9 +6,8 @@ import (
 	"net"
 	"net/http"
 
-	// "os"
-
-	// "golang.org/x/term"
+	"golang.org/x/term"
+	"os"
 
 	"github.com/amaury95/graphify"
 	libraryv1 "github.com/amaury95/graphify/example/domain/library/v1"
@@ -24,17 +23,17 @@ func main() {
 		password []byte
 		err      error
 	)
-	// // Prompt for username
-	// fmt.Print("Enter ArangoDB username: ")
-	// fmt.Scanln(&username)
+	// Prompt for username
+	fmt.Print("Enter ArangoDB username: ")
+	fmt.Scanln(&username)
 
-	// // Prompt for password
-	// fmt.Print("Enter password: ")
-	// password, err = term.ReadPassword(int(os.Stdin.Fd()))
-	// if err != nil {
-	// 	fmt.Println("Error reading password:", err)
-	// 	return
-	// }
+	// Prompt for password
+	fmt.Print("Enter password: ")
+	password, err = term.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		fmt.Println("Error reading password:", err)
+		return
+	}
 
 	// Define app context
 	ctx := graphify.DevelopmentContext(context.Background())
