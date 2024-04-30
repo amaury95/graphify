5b442eccfffd8c69687f70be5e5d1aacf8a79e01
Amaury Diaz <amauryuh@gmail.com>
2024-04-30 14:07:26 +0200

    Release v0.0.29

--- admin.go
+++ admin.go
@@ -71,7 +71,10 @@ func (g *graph) RestHandler(ctx context.Context) http.Handler {
 	resources.Delete("/:key", g.resourcesDeleteHandler)
 	resources.Get("/:key/:relation", g.resourcesRelationHandler)
 
-	
+	// Redirect to Dashboard on NotFound
+	admin.Get("/", func(c *fiber.Ctx) error {
+		return c.Redirect("/admin/dashboard")
+	})
 	return adaptor.FiberApp(router)
 }
 
